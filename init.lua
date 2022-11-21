a98b20312314d118b63f0f80f2c34489ccf1ee97
Jabrayilzade Ali <jabrayilzadeali@protonmail.com>
2022-11-22 01:09:10 +0400

    created base structure, options.lua and installed packer.

---
+++ init.lua
@@ -0,0 +1,4 @@
+require("ali.plugins_setup")
+require("ali.core.options")
+require("ali.core.keymaps")
+require("ali.core.colorscheme")
---
+++ lua/ali/plugins_setup.lua
@@ -0,0 +1,49 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({
+      'git',
+      'clone',
+      '--depth',
+      '1',
+      'https://github.com/wbthomason/packer.nvim',
+      install_path
+    })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins_setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+local status, packer = pcall(require, "packer")
+if not status then
+  return
+end
+
+packer.startup({function()
+  -- Your plugins here
+  -- Packer can manage itself
+  use "wbthomason/packer.nvim"
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end,
+config = {
+  display = {
+    open_fn = function()
+      return require('packer.util').float({ border = 'single' })
+    end
+  }
+}})
